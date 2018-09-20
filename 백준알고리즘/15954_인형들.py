7962fae46c5b201d9c53747ab583bfc290c6839e
kjy95 <rmarkd32145@naver.com>
2018-09-21 01:20:59 +0900

    no message

--- 백준알고리즘/15954_인형들.py
+++ 백준알고리즘/15954_인형들.py
@@ -1,24 +1,32 @@
-strnum = input().split(" ")
-dullNum = int(strnum[0])
-selectSize = int(strnum[1])
-likeNum = input().split(" ")
-m = 0
-flag = False
-for i in range(dullNum - selectSize+1):
-    tempsum= 0
-    temp2 = 0
-    for k in range(dullNum-selectSize-i+1):
-        for temp in range(selectSize+k):
-            tempsum += int(likeNum[temp+i])
-        
-        m = tempsum/(selectSize+k)
-        for temp in range(selectSize+k):
-            temp2 += (int(likeNum[temp+i])-m)**2
+import math
 
-        tempv = (temp2/(selectSize+k))**0.5
-        if not flag:
-            flag = True
-            v = tempv
-        if tempv<v:
-            v = tempv
-print(v)
+#input using map()
+#how to use map? --> map(f, iterable) f:funtion, iterable: iterable value
+
+def mean(arr):
+    return sum(arr)/len(arr)
+
+def v(arr):
+    v = 0
+    m = mean(arr)
+    for elm in arr:
+        v += (elm - m)**2
+    v = v/len(arr)
+    return v
+
+def std(arr):
+    return v(arr)**0.5
+
+def solution():
+    stds = []
+    n, k = map(int, input().split(" "))
+    likes = list(map(int, input().split(" ")))
+    for i in range(n-k+1):
+        for j in range(i+k,n+1):
+            stds.append(std(likes[i:j+1]))
+    print(min(stds))
+solution()
+
+#31500kb , 680ms
+#Python으로 구현하는 경우, 다른 언어들에 비해 실행 시간이 더 걸리기 때문에
+# , O(N3) 풀이를 구현하면 시간 초과를 받게 될 수 있습니다.
