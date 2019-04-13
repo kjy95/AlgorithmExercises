f0e3045270701a0a836094562cab9924049a27d1
DESKTOP-SAVNOE0\KJY <rmarkd32145@naver.com>
2019-04-13 16:26:29 +0900

    기초문제 정답.

---
+++ swtest/Greedy/baekjoon_4796.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int L, P, V, result, testCase = 1;
+    int divvy ,remain;
+    while(1){
+        cin >> L;
+        cin >> P;
+        cin >> V;
+        if(L == 0 && P == 0 && V == 0){
+            return 0;
+        }
+        divvy = V/P;
+        remain = V%P;
+        result = divvy*L ;
+        if(remain>=L){   
+            result+=L;
+        } else{
+            result+=remain;
+        }
+        cout << "Case "<< testCase <<": " << result << endl;
+        testCase ++;
+    }
+
+}
