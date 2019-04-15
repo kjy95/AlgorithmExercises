9e16f75cb82ba3b713a2e28c15f757393afe5749
DESKTOP-SAVNOE0\KJY <rmarkd32145@naver.com>
2019-04-16 00:05:20 +0900

    정답

--- swtest/완전탐색/baekjoon_10448.cpp
+++ swtest/완전탐색/baekjoon_10448.cpp
@@ -3,40 +3,28 @@
 using namespace std;
 
 int main(void){
-    int testcase, triArray[70];
+    int testcase, triArray[50];
     int testNum,i,j,k, sumResult = 0, result = -1;
     cin>> testcase;
-    for(i=1;i<70;i++){
+    for(i=1;i<50;i++){
         triArray[i] = i*(i+1)/2;
-    }
-    for(i=1;i<70;i++){
-        cout<< triArray[i] << endl;
     }
 
     while(testcase){
         cin>> testNum;
-        for(i=1;i<70;i++){
-            sumResult += triArray[i];
-            for(j=1;j<70;j++){
-                sumResult += triArray[j]; 
-                for(k=1;k<70
-                ;k++){
-                    sumResult += triArray[k];
-                    if(testNum == sumResult){
+        for(i=1;i<50;i++){
+            for(j=1;j<50;j++){
+                for(k=1;k<50;k++){
+                    if(testNum == triArray[i]+triArray[j]+triArray[k]){
                         result = 1;
                         cout<<result<<endl; 
                         break;
                     } 
-                    
-                    sumResult = 0;
                 }
-                sumResult = 0;
                 if(result == 1) break;
             }  
-                sumResult = 0; 
             if(result == 1) break;   
         }
-                sumResult = 0;
         if(result!=1) cout << 0<< endl; 
         result = -1;
         testcase--;
